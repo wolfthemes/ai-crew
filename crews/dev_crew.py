f8298c20cae6857f67cdf18c171edb918e951c65
wolfthemes <wolfthemes14@gmail.com>
2025-05-03 08:55:13 +0200

    feat: added long term memory to dev agent

--- crews/dev_crew.py
+++ crews/dev_crew.py
@@ -1,9 +1,22 @@
+import os
+from pathlib import Path
 from crewai import Crew, Process
 from agents.dev.dev_agent import dev_agent
+from crewai import Task
+from crewai.memory import LongTermMemory
+from crewai.memory.storage.ltm_sqlite_storage import LTMSQLiteStorage
 
-def dev_crew(task = dict):
+def dev_crew(task: Task) -> Crew:
+
+    base_dir = Path(__file__).resolve().parent.parent  # <- retourne au dossier "ai-crew"
+    db_path = base_dir / "data" / "db" / "coding_memory.db"
+
     return Crew(
         agents=[dev_agent],
         tasks=[task],
+        memory=True,
+        long_term_memory=LongTermMemory(
+            storage=LTMSQLiteStorage(db_path=str(db_path))
+        )
     )
     
